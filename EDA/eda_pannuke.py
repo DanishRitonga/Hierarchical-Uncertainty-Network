cd2ea487c48b004e75e3eedc9af86ff9c2b717ff
danishrtg <danishritonga@gmail.com>
2026-02-24 12:31:00 +0700

    update dep

--- EDA/eda_pannuke.py
+++ EDA/eda_pannuke.py
@@ -8,27 +8,36 @@ import pandas as pd
 from PIL import Image
 
 # %%
-parquet_file = Path("../data/PanNuke/data/fold1-00000-of-00001.parquet")
+data_dir = Path("../data/PanNuke/data")
+fold = 1
+parquet_file = Path(f"{data_dir}/fold{fold}-00000.parquet")
 
 df = pd.read_parquet(parquet_file)
 
 
 # %%
-def decode_image_bytes(byte_data):
-    """Converts raw PNG bytes into a NumPy array."""
+def _decode_image_bytes(byte_data):
     image = Image.open(io.BytesIO(byte_data))
     return np.array(image)
 
 
 # %%
-row = df.iloc[0]
-raw_bytes = row["instances"][0]["bytes"]
-image_file = io.BytesIO(raw_bytes)
+def decode_roi_bytes(df, row_index):
+    row = df.iloc[row_index]
+    byte_data = row["image"]["bytes"]
 
-img = Image.open(image_file)
+    return _decode_image_bytes(byte_data)
+
+
+# %%
+row = df.iloc[0]
+raw_bytes = row["image"]["bytes"]
+
+# img = _decode_image_bytes(raw_bytes)
+img = decode_roi_bytes(df, 1)
 
 plt.figure(figsize=(5, 5))
-plt.imshow(img)
+plt.imshow(img, cmap="gray")
 plt.axis("off")  # Hide the axis ticks
 plt.title(f"PanNuke H&E Image (Tissue: {row['tissue']})")
 plt.show()
